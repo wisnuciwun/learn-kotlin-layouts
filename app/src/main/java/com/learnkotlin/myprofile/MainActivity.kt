73507548041f9a69b5dc88c8ad5654ccb027489b
Wisnuadiwardhana <wisnu@tokodistributor.com>
2022-07-18 16:36:06 +0700

    learn part 3 - same activity_main but larger screen

--- app/src/main/java/com/learnkotlin/myprofile/MainActivity.kt
+++ app/src/main/java/com/learnkotlin/myprofile/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.learnkotlin.myprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawable
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val image = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val round = RoundedBitmapDrawableFactory.create(resources, image)
+        round.cornerRadius = 20f // float
+        img_avatar.setImageDrawable(round)
     }
 }
